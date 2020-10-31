751ce01e95a440a1a5f8af0e7e67a6a10b83d795
Imon <masudalimran92@gmail.com>
2020-10-31 16:32:56 +0600

    just checking

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -76,7 +76,7 @@
 					<!-- Logo -->
 					<div class="col-lg-2 col-sm-3 col-3 order-1">
 						<div class="logo_container">
-							<div class="logo"><a href="{{url('/')}}">IMON's</a></div>
+							<div class="logo"><a href="{{url('/')}}">BISMIB</a></div>
 						</div>
 					</div>
 
