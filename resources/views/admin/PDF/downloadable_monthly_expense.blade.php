6834b9d3d32ff7cd3e328762ea18b86e497bd8ab
masudalimran <masudalimran92@gmail.com>
2021-02-14 19:49:45 +0600

    update

--- resources/views/admin/PDF/downloadable_monthly_expense.blade.php
+++ resources/views/admin/PDF/downloadable_monthly_expense.blade.php
@@ -30,7 +30,7 @@ tr:nth-child(even) {
         <div style="display:inline; float:left; z-index: 9999">
             <img src="{{asset('public/backend/img/BISMIB TECHNOLOGY_Master color.png')}}" alt="bismib_logo" style="height: 180xp; width: 200px;">
         </div>
-        <h1 style="text-align: center; line-height: 35%; margin-top: -5px;"> <b style="color: blue; font-size: 20px;"> BISMIB TECHNOLOGY </b> </h1>
+        <h1 style="text-align: center; line-height: 35%; margin-top: -5px; position: fixed"> <b style="color: blue; font-size: 20px;"> BISMIB TECHNOLOGY </b> </h1>
 
         <h3 style="text-align: center; text-decoration: underline; margin-top: -5px;"> <b style="font-size: 16px;"> Expense Sheet</b></h3>
         @if ($pdf_data[2] == 0)
