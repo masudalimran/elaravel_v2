39fd3b6431bb8bc2bb1834bf0a854fa8ecc60ac7
masudalimran <masudalimran92@gmail.com>
2021-02-14 19:46:31 +0600

    updated

--- resources/views/admin/PDF/downloadable_monthly_expense.blade.php
+++ resources/views/admin/PDF/downloadable_monthly_expense.blade.php
@@ -28,11 +28,11 @@ tr:nth-child(even) {
     @endphp
     <div style="line-height: 10%;">
         <div style="display:inline; float:left;">
-            <img src="{{asset('public/backend/img/BISMIB TECHNOLOGY_Master color.png')}}" alt="bismib_logo" style="height: 150xp; width: 150px;">
+            <img src="{{asset('public/backend/img/BISMIB TECHNOLOGY_Master color.png')}}" alt="bismib_logo" style="height: 150xp; width: 200px;">
         </div>
-        <h1 style="text-align: center; line-height: 35%;"> <b style="color: blue; font-size: 20px;"> BISMIB TECHNOLOGY </b> </h1>
+        <h1 style="text-align: center; line-height: 35%; margin-top: -5px;"> <b style="color: blue; font-size: 20px;"> BISMIB TECHNOLOGY </b> </h1>
 
-        <h3 style="text-align: center; text-decoration: underline;"> <b style="font-size: 16px;"> Expense Sheet</b></h3>
+        <h3 style="text-align: center; text-decoration: underline; margin-top: -5px;"> <b style="font-size: 16px;"> Expense Sheet</b></h3>
         @if ($pdf_data[2] == 0)
             <h3 style="text-align: center; color: red; line-height: 5%"> January</h3>
             @php
